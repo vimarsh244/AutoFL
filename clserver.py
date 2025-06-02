50329db947c30efbbc430c911edab10ca16e800e
hchothani <harshdchothani@gmail.com>
2025-06-02 07:13:48 +0000

    added the modified FCL Provision

--- clserver.py
+++ clserver.py
@@ -5,13 +5,30 @@ from flwr.server.strategy import FedAvg
 
 from clutils.clmetrics import evaluate_metrics_aggregation_fn
 
+NUM_ROUNDS = 5
+NUM_CLIENTS = 5
+
+def fit_config(server_round: int):
+    """Return training configuration dict for each round.
+
+    Perform two rounds of training with one local epoch, increase to two local
+    epochs afterwards.
+    """
+    config = {
+        "server_round": server_round,  # The current round of federated learning
+        "local_epochs": 3, 
+        "num_rounds": NUM_ROUNDS
+    }
+    return config
+
 # Create FedAvg strategy
 strategy = FedAvg(
     fraction_fit=1.0,  # Sample 100% of available clients for training
     fraction_evaluate=1.0,  # Sample 50% of available clients for evaluation
-    min_fit_clients=5,  # Never sample less than 10 clients for training
-    min_evaluate_clients=5,  # Never sample less than 5 clients for evaluation
-    min_available_clients=5,  # Wait until all 10 clients are available
+    min_fit_clients=NUM_CLIENTS,  # Never sample less than 10 clients for training
+    min_evaluate_clients=NUM_CLIENTS,  # Never sample less than 5 clients for evaluation
+    min_available_clients=NUM_CLIENTS,  # Wait until all 10 clients are available
+    on_fit_config_fn=fit_config,
     evaluate_metrics_aggregation_fn=evaluate_metrics_aggregation_fn
 )
 
@@ -24,7 +41,7 @@ def server_fn(context: Context) -> ServerAppComponents:
     """
 
     # Configure the server for 5 rounds of training
-    config = ServerConfig(num_rounds=5)
+    config = ServerConfig(NUM_ROUNDS)
 
     return ServerAppComponents(strategy=strategy, config=config)
 
